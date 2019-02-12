6073cb77e60384dfc4ec94ae6fd438001beaa16e
Mariusz Zawadzki <mariusz.r.zawadzki@gmail.com>
2019-02-12 19:49:21 +0100

    dodaj rysowanie ramek

--- src/pdf/language-cards-pdf.js
+++ src/pdf/language-cards-pdf.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import {pixel} from "../consts";
 
 const BORDER = [true, true, true, true];
 const colWidth = 45 * 2.87;
@@ -6,7 +7,7 @@ const tablePadding = 7;
 const boxSize = colWidth - 10;
 const imgWidth = 172;
 
-export function getLanguagePdf(){
+export function getLanguagePdf() {
     const title = 'karty.pdf';
     try {
         const docDefinition = {
@@ -16,7 +17,8 @@ export function getLanguagePdf(){
             styles: {
                 tableExample: {
                     margin: [-20, 0, 0, 0],
-                    alignment: 'center'
+                    alignment: 'center',
+                    border: [false, false, false, false]
                 }
             },
         };
@@ -26,7 +28,7 @@ export function getLanguagePdf(){
                 style: 'tableExample',
                 table: {
                     widths: [colWidth, colWidth, colWidth],
-                    heights: [boxSize,boxSize,boxSize,boxSize],
+                    heights: [boxSize, boxSize, boxSize, boxSize],
                     body: getLanguageGrid()
                 },
                 pageBreak: 'after',
@@ -34,7 +36,24 @@ export function getLanguagePdf(){
                     paddingLeft: () => tablePadding,
                     paddingRight: () => tablePadding,
                     paddingTop: () => tablePadding,
-                    paddingBottom: () => tablePadding
+                    paddingBottom: () => tablePadding,
+                    // fillColor: function (i, node) {
+                    //     return (i % 2 === 0) ? '#fafafe' : null;
+                    // },
+                    hLineColor: function (i, node) {
+                        // return (i === 0 || i === node.table.body.length) ? 'black' :
+                            return '#ccc';
+                    },
+                    vLineColor: function (i, node) {
+                        // return (i === 0 || i === node.table.widths.length) ? 'black' :
+                            return '#ccc';
+                    },
+                    hLineWidth: function (i, node) {
+                        return 2;
+                    },
+                    vLineWidth: function (i) {
+                        return 2;
+                    }
                 }
             }
         ];
@@ -50,39 +69,71 @@ export function getLanguagePdf(){
         window.pdfMake.createPdf(docDefinition).getDataUrl(function (outDoc) {
             document.getElementById('pdfV').src = outDoc;
         });
-
-    }
-    catch (e) {
+    } catch (e) {
         console.log(e);
         // callback(false);
     }
 }
 
 export function getLanguageGrid() {
-    const times = _.times(4, (row)=>{
+    const times = _.times(4, (row) => {
         return getRow(row);
     });
     console.log(JSON.stringify(times));
     return times
 }
-function getRow(row){
+
+function getRow(row, content) {
+    content = {
+        "type": "text",
+        "data": [
+            "czarownica",
+            "kot",
+            "miotła"
+        ]
+    }
+    if (content.type === "image") {
+        content.data = content.data.map((dataUrl) => {
+            return {
+                image: dataUrl,
+                width: boxSize,
+                height: boxSize
+            }
+        })
+    } else if (content.type === "text") {
+        content.data = content.data.map((text) => {
+            const margin = parseInt(boxSize / 2 - tablePadding * 2 * Math.max(1, parseInt(text.length / 10)));
+            return {text: [text], fontSize: 23, margin: [0, margin, 0, 0]}
+        })
+    } else {
+        content.data = _.times(3, () => {
+            return {
+                image: pixel,
+                width: boxSize,
+                height: boxSize
+            }
+        })
+    }
     return _.times(3, (col) => {
-        const text = "czarownica nożyczki";
-        console.log("text leng is "+ text.length + "and shorted is");
-        const margin = parseInt(boxSize / 2 - tablePadding *2* (text.length / 10));
-        console.log("margin is " + margin)
         return [{
-                table: {
-                    border: BORDER,
-                    widths:  [boxSize],
-                    heights: boxSize,
-                    body: [
-                        [
-                            [
-                                {text : [text], fontSize: 22, margin: [0, margin, 0, 0]}
-                        ]
-                    ]]
+            table: {
+                border: [false, false, false, false],
+                widths: [boxSize],
+                heights: boxSize,
+                body: [
+                    [content.data[col]]
+                ]
+            },
+            layout: {
+                hLineColor: function (i, node) {
+                    // return (i === 0 || i === node.table.body.length) ? 'black' :
+                    return "#fff";
+                },
+                vLineColor: function (i, node) {
+                    // return (i === 0 || i === node.table.widths.length) ? 'black' :
+                    return "#fff";
                 }
+            }
         }
 
         ];
